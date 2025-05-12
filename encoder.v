65169f87caf3a648481d62d82fd6ef0aa471b485
Matheus <matheusamg@gmail.com>
2025-05-12 17:33:43 -0300

    primeira tentativa

--- encoder.v
+++ encoder.v
@@ -8,5 +8,79 @@ module Encoder (
     output reg A,
     output reg B
 );
-    
+
+reg [1:0] current_state;
+
+localparam [1:0] S0 = 2'b00,
+                 S1 = 2'b01,
+                 S2 = 2'b10,
+                 S3 = 2'b11;
+
+always @(posedge clk, negedge rst_n) begin
+    if(~rst_n) begin
+        A <= 0;
+        B <= 0;
+        current_state <= 0;
+    end
+
+    else begin
+        if(horario && ~antihorario) begin
+            case (current_state)
+                S0: begin
+                    current_state <= S2;
+                    A <= S2[1];
+                    B <= S2[0];
+                end 
+                S1: begin
+                    current_state <= S0;
+                    A <= S0[1];
+                    B <= S0[0]; 
+                end 
+                S2: begin
+                    current_state <= S3;
+                    A <= S3[1];
+                    B <= S3[0];
+                end 
+                S3: begin
+                    current_state <= S1;
+                    A <= S1[1];
+                    B <= S1[0];
+                end 
+            endcase
+        end
+
+        else if(antihorario && ~horario) begin
+            case (current_state)
+                S0: begin
+                    current_state <= S1;
+                    A <= S1[1];
+                    B <= S1[0];
+                end 
+                S1: begin
+                    current_state <= S3;
+                    A <= S3[1];
+                    B <= S3[0]; 
+                end 
+                S2: begin
+                    current_state <= S0;
+                    A <= S0[1];
+                    B <= S0[0];
+                end 
+                S3: begin
+                    current_state <= S2;
+                    A <= S2[1];
+                    B <= S2[0];
+                end 
+            endcase
+        end
+
+        else begin
+            current_state <= current_state;
+            A <= current_state[1];
+            B <= current_state[0];
+        end
+    end
+
+end
+
 endmodule
--- read_encoder.v
+++ read_encoder.v
@@ -7,5 +7,40 @@ module Read_Encoder (
 
     output reg [1:0] dir
 );
-    
+
+    reg A_anterior;
+    reg B_anterior;
+
+    always @(posedge clk or negedge rst_n) begin
+        if(~rst_n) begin
+            dir <= 2'b00;
+            A_anterior <= 1'b0;
+            B_anterior <= 1'b0;
+        end else begin
+            // Detecta sentido anti-horário
+            if ((A_anterior == 0 && B_anterior == 0 && A == 1 && B == 0) ||
+                (A_anterior == 1 && B_anterior == 0 && A == 1 && B == 1) ||
+                (A_anterior == 1 && B_anterior == 1 && A == 0 && B == 1) ||
+                (A_anterior == 0 && B_anterior == 1 && A == 0 && B == 0)) begin
+                    dir <= 2'b01;
+            end
+
+            // Detecta sentido horário
+            else if ((A_anterior == 0 && B_anterior == 0 && A == 0 && B == 1) ||
+                     (A_anterior == 0 && B_anterior == 1 && A == 1 && B == 1) ||
+                     (A_anterior == 1 && B_anterior == 1 && A == 1 && B == 0) ||
+                     (A_anterior == 1 && B_anterior == 0 && A == 0 && B == 0)) begin
+                dir <= 2'b10;
+            end
+
+            else begin
+                dir <= 2'b00;
+            end
+
+            // Atualiza estados anteriores
+            A_anterior <= A;
+            B_anterior <= B;
+        end
+    end
+
 endmodule
